d2fd17c91a006d57e050b4c4028b9f8a90ca47f1
Abir Hasan AHK <ahksoft33@gmail.com>
2026-07-24 20:48:13 +0600

    Update vendorsetup.sh and AndroidProducts.mk for TWRP 14+ lunch format

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,15 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_raven.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_raven-user \
-    omni_raven-userdebug \
-    omni_raven-eng
+    omni_raven-14-user \
+    omni_raven-14-userdebug \
+    omni_raven-14-eng \
+    omni_raven-15-user \
+    omni_raven-15-userdebug \
+    omni_raven-15-eng \
+    omni_raven-16-user \
+    omni_raven-16-userdebug \
+    omni_raven-16-eng \
+    omni_raven-17-user \
+    omni_raven-17-userdebug \
+    omni_raven-17-eng
