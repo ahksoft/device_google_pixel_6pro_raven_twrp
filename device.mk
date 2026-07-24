772ce15a88f5d4a70f0aba007b00240a7bbd1fb1
Abir Hasan AHK <ahksoft33@gmail.com>
2026-07-24 22:08:30 +0600

    Remove obsolete PRODUCT_STATIC_BOOT_CONTROL_HAL

--- device.mk
+++ device.mk
@@ -21,12 +21,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.gs101
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.gs101 \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
