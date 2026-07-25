7e560fad05e04092dc33edd94da5694fe2bd7687
ahksoft <ahksoft@users.noreply.github.com>
2026-07-25 00:50:59 +0000

    Fix soong build hang: strip unnecessary packages and APEX
    - Remove OTA packages from device.mk (otapreopt_script, update_engine, etc.)
      that don't exist in twrp-14.1 tree and cause soong to spin
    - Remove DEXPREOPT_GENERATE_APEX_IMAGE (not needed for recovery build)
    - Keep only bootctrl.gs101 in PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -6,24 +6,6 @@
 #
 
 LOCAL_PATH := device/google/raven
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
     bootctrl.gs101
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
